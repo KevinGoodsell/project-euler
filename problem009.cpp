a12dae5669dd94f7f45ae3220144d0573283a1ac
Kevin Goodsell <kevin-opensource@omegacrash.net>
2011-05-25 14:08:22 -0700

    Add problem 9

---
+++ problem009.cpp
@@ -0,0 +1,39 @@
+/* Project Euler #9
+ *
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+ * which,
+ * a^2 + b^2 = c^2
+ *
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+#include <iostream>
+
+bool is_ptriplet(int a, int b, int c)
+{
+    return a*a + b*b == c*c;
+}
+
+int main()
+{
+    int a, b, c;
+
+    for (a = 1; a < 998; a++) {
+        for (b = a+1; ; b++) {
+            c = 1000 - a - b;
+            if (c < b || c < a) break;
+
+            if (a*a + b*b == c*c) {
+                std::cout << a << ' ' << b << ' ' << c << std::endl;
+                std::cout << (a*b*c) << std::endl;
+                return 0;
+            }
+        }
+    }
+
+
+    return 0;
+}
