b9e71620a0cd99c1b18b45d5d2256dfab388bdf3
Kevin Goodsell <kevin-opensource@omegacrash.net>
2011-06-03 11:12:50 -0700

    Update problem 40.
    This is an efficient approach, but still rather ugly.

--- problem040.cpp
+++ problem040.cpp
@@ -17,8 +17,42 @@
 #include <sstream>
 #include <string>
 
+int get_digit(long pos)
+{
+    int group = 1;
+    long group_start = 1; // group ^ 10
+
+    while (true) {
+        long group_size = 9 * group * group_start;
+        if (pos <= group_size) break;
+        pos -= group_size;
+        group++;
+        group_start *= 10;
+    }
+
+    // val is the number before appending into the irrational
+    long val = group_start + (pos-1) / group;
+    int digit_pos = group - 1 - (pos-1) % group;
+    while (digit_pos > 0) {
+        val /= 10;
+        digit_pos--;
+    }
+    return val % 10;
+}
+
 int main()
 {
+    long result = get_digit(1)
+                * get_digit(10)
+                * get_digit(100)
+                * get_digit(1000)
+                * get_digit(10000)
+                * get_digit(100000)
+                * get_digit(1000000);
+
+    std::cout << result << std::endl;
+
+    /*
     // Ugly brute-force approach
     std::ostringstream sout;
     std::string val;
@@ -36,6 +70,7 @@ int main()
         (val[1000000] - '0');
 
     std::cout << result << std::endl;
+    */
 
     return 0;
 }
